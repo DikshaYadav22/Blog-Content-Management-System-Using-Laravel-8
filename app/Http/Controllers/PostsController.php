1979a13c01e320b0709d5cafc470f9438b03dcfd
DikshaYadav22 <diksha.yadav022@gmail.com>
2022-04-13 10:57:33 +0530

    creating the post view

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,32 +6,19 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
         //
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         //
